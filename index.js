664eae899964481d63b2f0358cc3f01797b89771
Payal760 <payalardeshana96@gmail.com>
2024-12-07 18:46:01 +0530

    signup/login page modified

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function signup(userName){
      // Check if the username is already in the array
      if(registerdUser.includes(userName)){
         return "User Already Regsistered, Please Login "
+        //// Add the new username to the array
      }else{
         registerdUser.push(userName);
         return "Signup Successful, Please Login"
@@ -10,3 +11,24 @@ function signup(userName){
 }
 console.log(signup("Sarah"));
 console.log(signup("Michelle"))
+
+
+function login(userName, password){
+    let registerdUser = ["Payal", "Abogail","Annabelle","Rachel","Sarah"];
+     // Check if the username is in the array
+     if(registerdUser.includes(userName)){
+           // Check if the password is correct
+           if(password=="Emp@123"){
+            return "Login Successful...";
+           }else{
+            return "User Not Found, Please Signup";
+           }
+        }else{
+            return "Wrong Password...."
+           }
+     }
+
+
+console.log(login("Payal", "Emp@123")); // Login Successful...
+console.log(login("Abigail", "wrongpassword")); // Wrong Password....
+console.log(login("Rapheal", "Emp@123")); // User Not Found, Please Signup
